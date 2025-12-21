8a45378474947f7f4492c4b3d3995372747d2215
ATO-O <oh.look.id@proton.me>
2025-12-21 17:17:19 +0530

    changing title, logo and background color.

--- game.py
+++ game.py
@@ -1,14 +1,25 @@
 import pygame
 
-#initializing pygame
+# initializing pygame
 pygame.init()
 
-#setting screen
+# setting screen
 screen = pygame.display.set_mode((800, 600))
 running = True
 
-#game loop
+#display text & icons
+pygame.display.set_caption("Magnatiz")
+icon = pygame.image.load("icon.png")
+pygame.display.set_icon(icon)
+
+# game loop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    #rgb - theme of screen
+    screen.fill((0, 0, 255))
+    pygame.display.update()
+
+
