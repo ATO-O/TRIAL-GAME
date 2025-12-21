3aefd6cfcf4117ca4f1be3ed5f9e86943c216477
ATO-O <oh.look.id@proton.me>
2025-12-21 18:36:44 +0530

    adding images into projects

--- game.py
+++ game.py
@@ -4,22 +4,32 @@ import pygame
 pygame.init()
 
 # setting screen
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((1000, 600))
 running = True
 
-#display text & icons
+# display text & icons
 pygame.display.set_caption("Magnatiz")
 icon = pygame.image.load("icon.png")
 pygame.display.set_icon(icon)
 
+# player icon
+playerImg = pygame.image.load("player.png")
+playerX = 450
+playerY = 480
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+# background icon
+# background = pygame.image.load("background.png")
+
 # game loop
 while running:
+    # rgb - theme of screen
+    screen.fill((0, 0, 255))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    #rgb - theme of screen
-    screen.fill((0, 0, 255))
+    player()
     pygame.display.update()
-
-
