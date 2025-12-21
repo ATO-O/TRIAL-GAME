dbc3f9e447229d64f6c38c2dcd4f5f405625eef4
ATO-O <oh.look.id@proton.me>
2025-12-21 18:58:13 +0530

    adding movement mechanics and keyboard control to the dame

--- game.py
+++ game.py
@@ -16,9 +16,10 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load("player.png")
 playerX = 450
 playerY = 480
+playerX_change = 0
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 # background icon
 # background = pygame.image.load("background.png")
@@ -31,5 +32,21 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    player()
+        #keystroke movevent for the player.
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -1
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+
+    playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 870:
+        playerX = 870
+    player(playerX, playerY)
     pygame.display.update()
